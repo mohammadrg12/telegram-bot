8051c5677167045da9339a142be559c6c015dee1
mohammadrg12 <mrg.bee20@gmail.com>
2025-09-08 20:35:51 +0330

    Add bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import random
+
+TOKEN =7875997444:AAEN15vuATcEVUAjkocksKLK58cWQ-uJ3F0
+
+members = []
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! برای شرکت در قرعه‌کشی دستور /join را بزنید 🎉")
+
+async def join(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    username = user.username or str(user.id)
+    if username not in members:
+        members.append(username)
+        await update.message.reply_text(f"@{username} در قرعه‌کشی ثبت شد ✅")
+    else:
+        await update.message.reply_text("قبلاً ثبت‌نام کرده‌ای 🙂")
+
+async def draw(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if members:
+        winner = random.choice(members)
+        await update.message.reply_text(f"🎊 برنده: @{winner}")
+    else:
+        await update.message.reply_text("❌ هنوز کسی ثبت‌نام نکرده!")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("join", join))
+    app.add_handler(CommandHandler("draw", draw))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
